9fcaaef7ad9e6c932a905385b7a9b26c6b3040b2
kazurayam <kazuaki.urayama@gmail.com>
2025-01-22 21:53:53 +0900

    updates

--- Include/scripts/groovy/com/kazurayam/ks/ExecutionSettingsModifierTest.groovy
+++ Include/scripts/groovy/com/kazurayam/ks/ExecutionSettingsModifierTest.groovy
@@ -1,15 +1,38 @@
 package com.kazurayam.ks
 
-import static org.hamcrest.CoreMatchers.*
+import static org.hamcrest.CoreMatchers.is
 import static org.junit.Assert.*
 
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import java.nio.file.Files
+import java.nio.file.Path
 
 @RunWith(JUnit4.class)
 public class ExecutionSettingsModifierTest {
 
+	@Test
+	void test_getExecutionPropertiesFile() {
+		Path file = ExecutionSettingsModifier.getExecutionPropertiesFile()
+		assertTrue(Files.exists(file))	
+	}
+	
+	@Test
+	void test_getExecutionProperties() {
+		Properties props = ExecutionSettingsModifier.getExecutionProperties()
+		assertNotNull(props)
+		assertEquals('25', props.get("execution.default.timeout"))	
+	}
+	
+	@Test
+	void test_modifyExecutionProerty() {
+		ExecutionSettingsModifier.modifyExecutionProperty("execution.default.timeout", "13")
+		//
+		Properties props = ExecutionSettingsModifier.getExecutionProperties()
+		assertEquals('13', props.get("execution.default.timeout"))
+	}
+	
 	@Test
 	void testFoo() {
 		int expected = 21
--- Keywords/com/kazurayam/ks/ExecutionSettingsModifier.groovy
+++ Keywords/com/kazurayam/ks/ExecutionSettingsModifier.groovy
@@ -1,12 +1,39 @@
 package com.kazurayam.ks
 
+import com.kms.katalon.core.configuration.RunConfiguration
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
 public class ExecutionSettingsModifier {
 
 	static void modifyExecutionDefaultTimeout(int timeout) {
 		;
 	}
-	
+
 	static int foo(int value) {
 		return value
 	}
+	
+	static Path getExecutionPropertiesFile() {
+		Path projectDir = Paths.get(RunConfiguration.getProjectDir())
+		Path propertiesFile = projectDir.resolve("settings/internal/com.kms.katalon.execution.properties")
+		assert Files.exists(propertiesFile)
+		return propertiesFile
+	}
+	
+	static Properties getExecutionProperties() {
+		Properties props = new Properties()
+		props.load(Files.newInputStream(ExecutionSettingsModifier.getExecutionPropertiesFile()))
+		return props
+	}
+	
+	static void modifyExecutionProperty(String key, String value) {
+		Properties props = ExecutionSettingsModifier.getExecutionProperties()
+		props.setProperty(key, value)
+		Path propertiesFile = ExecutionSettingsModifier.getExecutionPropertiesFile()
+		OutputStream os = Files.newOutputStream(propertiesFile)
+		props.store(os, "")
+	}
+	
 }
